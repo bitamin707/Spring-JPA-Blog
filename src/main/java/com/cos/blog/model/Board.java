f43bd2327b5fa974a15857a0cced9127c750f61d
WonGyu <bitamin707@naver.com>
2022-11-10 17:47:35 +0900

    User, Board, Reply 테이블 생성

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,48 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+public class Board {
+
+	@Id //primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; //시퀀스
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob //대용량 데이터
+	private String content; //섬머노트 라이브러리
+	
+	@ColumnDefault("0")
+	private int count; //조회수
+
+	@ManyToOne //Many = Board, One = User, 한 계정에 여러 개 글 작성
+	@JoinColumn(name = "userid")
+	private User user; //작성자, FK
+	
+	@CreationTimestamp //시간 자동 입력
+	private Timestamp createDate;
+}
