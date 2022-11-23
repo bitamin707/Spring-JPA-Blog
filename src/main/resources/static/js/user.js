08f52025dcd6576d8bd04458b03831ec6dcfe3e7
WonGyu <bitamin707@naver.com>
2022-11-23 16:07:41 +0900

    회원가입, Ajax 요청 및 처리 구현

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -11,9 +11,23 @@ let index = {
 			username:$("#username").val(),
 			password:$("#password").val(),
 			email:$("#email").val()
-		}
+		};
 		
-		$.ajax().done().fail(); 
+		$.ajax({
+			//회원가입 수행 요청
+			type:"POST",
+			url:"/blog/api/user",
+			data:JSON.stringify(data), //http body 데이터
+			contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지
+			dataType:"json"
+		}).done(function(resp) {
+			console.log(resp);
+			alert("회원가입 완료");
+			
+			location.href="/blog/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		}); 
 	}
 }
 
