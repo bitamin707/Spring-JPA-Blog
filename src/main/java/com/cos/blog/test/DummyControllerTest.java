d7522a75e3fdd9f7fbcb718ad1bf872f3bc4856d
WonGyu <bitamin707@naver.com>
2022-11-30 18:03:14 +0900

    글목록 페이징

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -67,11 +67,11 @@ public class DummyControllerTest {
 	
 	//페이징
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable) {
+	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable) {
 		Page<User> pagingUser = userRepository.findAll(pageable);
 		
 		List<User> users = pagingUser.getContent();
-		return users;
+		return pagingUser;
 	}
 	
 	//Select
